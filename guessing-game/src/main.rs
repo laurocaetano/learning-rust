40fb923f655e07a03e84c1e73380b523601cc824
Lauro Caetano <laurocaetano1@gmail.com>
2021-01-01 10:41:36 +0100

    Use Struct to improve the game logic validation

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,5 +1,19 @@
 use rand::{thread_rng, Rng};
 
+struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    fn new(value: u32) -> Guess {
+        if value <= 100 {
+            Guess { value }
+        } else {
+            panic!("The user guess must be a number between 1 and 100.");
+        }
+    }
+}
+
 fn main() {
     println!("Enter a guess from 1 to 100:");
 
@@ -9,19 +23,19 @@ fn main() {
         .read_line(&mut user_guess)
         .expect("Please enter a valid String");
 
-    let user_guess: u32 = match user_guess.trim().parse() {
-        Ok(v) => v,
-        Err(_) => panic!("Expected a value between 1 and 100. Please enter a valid number."),
+    let guess = match user_guess.trim().parse() {
+        Ok(v) => Guess::new(v),
+        Err(_) => panic!("Please enter a number between 1 and 100."),
     };
 
-    let random_guess: u32 = thread_rng().gen_range(1..=100);
+    let random_number: u32 = thread_rng().gen_range(1..=100);
 
-    if user_guess == random_guess {
+    if guess.value == random_number {
         println!("You've guessed correctly!");
     } else {
         println!(
             "You did not guess correctly. The random number was: {}.",
-            random_guess
+            random_number
         );
     }
 }
